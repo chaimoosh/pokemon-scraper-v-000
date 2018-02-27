accdbae0b2ee8e1d417ca1ca704321ac3ab9452d
Chaim Reisman <crreisman@gmail.com>
2018-02-27 15:28:37 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
-  attr_accessor :name, :type, :db
+  attr_accessor :name, :type, :db, :id
   
-  def initialize(name:, type:, db:)
+  def initialize(name:, type:, db:, id:)
     local_variables.each do |k|
     v = eval(k.to_s)
     instance_variable_set("@#{k}", v) unless v.nil?
