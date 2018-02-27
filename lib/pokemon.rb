fb83de76e7b0539afd42be110b44326884f25265
Chaim Reisman <crreisman@gmail.com>
2018-02-27 15:29:50 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,8 +3,12 @@ class Pokemon
   
   def initialize(name:, type:, db:, id:)
     local_variables.each do |k|
-    v = eval(k.to_s)
-    instance_variable_set("@#{k}", v) unless v.nil?
+      v = eval(k.to_s)
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
   end
+  
+  def self.save 
+    
   end 
 end
